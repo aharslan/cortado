185ae7ab129e583f128713224bad21900f23a4b4
Ahmad Arslan <arslan0157@gmail.com>
2024-07-09 13:57:13 +0200

    reformatting

--- src/backend/endpoints/remove_variant_from_process_model.py
+++ src/backend/endpoints/remove_variant_from_process_model.py
@@ -1,6 +1,7 @@
 from typing import List
 from cortado_core.negative_process_model_repair.trace_remover import (
-    apply_negative_process_model_repair, apply_frequency_rating_based_negative_process_model_repair,
+    apply_negative_process_model_repair,
+    apply_frequency_rating_based_negative_process_model_repair,
 )
 from cortado_core.utils.trace import TypedTrace
 from backend_utilities.process_tree_conversion import (
@@ -17,8 +18,12 @@ def remove_variant_from_process_model(
     frozen_subtrees: List[ProcessTree]
     pt, frozen_subtrees = dict_to_process_tree(pt_dict)
 
-    updated_tree, approach_used, percentage_positive_variants_conforming, resulting_tree_edit_distance, applied_rules = apply_negative_process_model_repair(
-        pt, negative_variant, positive_variants
-    )
+    (
+        updated_tree,
+        approach_used,
+        percentage_positive_variants_conforming,
+        resulting_tree_edit_distance,
+        applied_rules,
+    ) = apply_negative_process_model_repair(pt, negative_variant, positive_variants)
     res = process_tree_to_dict(updated_tree)
     return res
